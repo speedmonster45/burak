9238efa9649c30fa0db94bf2189ee4b290475460
tuzizila11 <41533496+tuzizila11@users.noreply.github.com>
2018-07-24 10:35:30 -0400

    Update Carddeck.h

--- Carddeck.h
+++ Carddeck.h
@@ -12,12 +12,16 @@ class CardDeck
     string numValue[13] = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
     string suit[4] = {"H","S","D","C"};
     vector<string> randDeck;
-    vector<string> currentCards;
+    vector<string> usedCards;
+    static int rndCount;
+    string trumpCard;
 
   public:
     void createDeck();
     void shuffleDeck();
     string getCard();
     bool isGreater(string& card1, string& card2);
+    void setTrumpCard();
+    //void outputDeck();
 };
 #endif
